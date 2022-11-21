be33b6ff4d2de1b08ea7fa508de43fc5c22e4cde
studmanj <bjackso3@hotmail.com>
2022-11-20 21:40:59 -0700

    practice

---
+++ index.js
@@ -0,0 +1,6 @@
+const descriptions = document.querySelectorAll('.description-display')
+for (let desc of descriptions.values()) {
+    let content = desc.innerText;
+    console.log(content);
+  }
+  
